8479396fa0149f4c27dc6338539cf51fe2cb7199
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-09-17 14:05:34 +0430

    test: setup units

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+global.React = React
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from '@testing-library/react';
+import { newTodo, Todo } from './App';
+
+test('Should return an object with new name by calling newTodo', () => {
+  const result = newTodo('My new Item')
+
+  expect(result).toBeDefined()
+  expect(result).toHaveProperty('id')
+  expect(result).toHaveProperty('name')
+  expect(result).toHaveProperty('complete')
+  expect(Object.values(result)).toContain('My new Item')
 });
+
+test('Should render Todo component', async () => {
+  const todoItem = newTodo('My new Item')
+  const dispatcher = jest.fn(() => [])
+
+  render(<Todo todo={todoItem} dispatch={dispatcher} />)
+
+  const itemValue = screen.getByText(/My new Item/i);
+  expect(itemValue).toBeInTheDOM()
+})
