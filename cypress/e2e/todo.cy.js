7249b1f01b181f6edf5f2433933c2683cae5e270
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-09-17 22:02:11 +0430

    test: todo

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -5,6 +5,10 @@ describe('empty spec', () => {
 
 
   it('should add item', () => {
+    const newTodo = 'do dishes'
+    cy.get('input[name=new]').should('exist').should('be.visible').type(newTodo)
+    cy.get('button[name="add-item"]').should('exist').should('be.visible').click()
 
+    expect(cy.get)
   })
 })
